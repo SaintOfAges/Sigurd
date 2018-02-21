9c84c30df58c8d59e4a4db04a35aab46fb1d7d92
SaintOfAges <30766543+SaintOfAges@users.noreply.github.com>
2018-02-21 12:31:23 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,28 @@ client.on('message', message => {
     	message.reply('My Papa!');
   	}
 });
-            
+
+client.on('message', message => {
+    if (message.content === 'bored') {
+    	message.reply('Lets do something!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'lolmayo') {
+    	message.reply('Hey I love that! <3');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'lol') {
+    	message.reply('Fufufufu!!!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '@L_Yagami#3482') {
+    	message.reply('I feel homesick sometimes ;-;');
+  	}
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
